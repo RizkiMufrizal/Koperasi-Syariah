dc56dfd07cf29507270f687568f673e0b1cf9f00
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-13 17:38:06 +0700

    initial sql query tapi masih ada beberapa yang belum selesai

---
+++ Koperasi_Syariah.sql
@@ -0,0 +1,111 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * @Since Agu 13, 2016
+ * @Time 17:21:30 PM
+ * @Encoding UTF-8
+ * @Project Koperasi-Syariah
+ * tabel tb_pembiayaan belum selesai, dan column biaya_administrasi
+ */
+
+CREATE DATABASE koperasi_syariah;
+USE koperasi_syariah;
+
+CREATE TABLE tb_user(
+	username VARCHAR(50) NOT NULL PRIMARY KEY,
+	password VARCHAR(100) NOT NULL
+)ENGINE=INNODB;
+
+CREATE TABLE tb_anggota(
+	id_anggota VARCHAR(50) NOT NULL PRIMARY KEY,
+	nama VARCHAR(50) NOT NULL,
+	tanggal_pendaftaran DATE NOT NULL,
+	telepon CHAR(15) NOT NULL,
+	tempat_lahir VARCHAR(50) NOT NULL,
+	tanggal_lahir DATE NOT NULL,
+	jenis_kelamin CHAR(6) NOT NULL,
+	rembug VARCHAR(50) NOT NULL,
+	setoran_awal DECIMAL NOT NULL,
+	alamat TEXT NOT NULL,
+	status TINYINT NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	FOREIGN KEY(username) REFERENCES tb_user(username)
+)ENGINE=INNODB;
+
+CREATE TABLE tb_simpanan_anggota(
+	id_simpanan_anggota VARCHAR(50) NOT NULL PRIMARY KEY,
+	tanggal_transaksi DATE NOT NULL,
+	simpanan_pokok DECIMAL NOT NULL,
+	simpanan_sukarela DECIMAL NOT NULL,
+	simpanan_hari_raya DECIMAL NOT NULL,
+	simpanan_wajib DECIMAL NOT NULL,
+	simpanan_pendidikan DECIMAL NOT NULL,
+	pengambilan DECIMAL NOT NULL,
+	saldo DECIMAL NOT NULL,
+	id_anggota VARCHAR(50) NOT NULL,
+	FOREIGN KEY(id_anggota) REFERENCES tb_anggota(id_anggota)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)ENGINE=INNODB;
+
+--- masih belum
+
+CREATE TABLE tb_pembiayaan(
+	id_pembiayaan VARCHAR(50) NOT NULL PRIMARY KEY,
+	tanggal_peminjaman DATE NOT NULL,
+	tanggal_jatuh_tempo DATE NOT NULL,
+	pembiayaan DECIMAL NOT NULL,
+	biaya_administrasi DECIMAL NOT NULL,
+	jenis_pembiayaan CHAR(15) NOT NULL,
+	id_anggota VARCHAR(50) NOT NULL,
+	FOREIGN KEY(id_anggota) REFERENCES tb_anggota(id_anggota)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)ENGINE=INNODB;
+
+--- masih belum
+
+CREATE TABLE tb_angsuran_pembiayaan(
+	id_angsuran_pembiayaan VARCHAR(50) NOT NULL PRIMARY KEY,
+	tanggal_pembayaran_angsuran DATE NOT NULL,
+	bagi_hasil_koperasi DECIMAL NOT NULL,
+	bagi_hasil_anggota DECIMAL NOT NULL,
+	biaya_administrasi DECIMAL NOT NULL,
+	keterangan CHAR(15) NOT NULL,
+	id_pembiayaan VARCHAR(50) NOT NULL,
+	FOREIGN KEY(id_pembiayaan) REFERENCES tb_pembiayaan(id_pembiayaan)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)ENGINE=INNODB;
+
+CREATE TABLE tb_peminjaman_instan(
+	id_peminjaman_instan VARCHAR(50) NOT NULL PRIMARY KEY,
+	tanggal_peminjaman DATE NOT NULL,
+	tanggal_jatuh_tempo DATE NOT NULL,
+	tanggal_pengembalian DATE NOT NULL,
+	pinjaman DECIMAL NOT NULL,
+	pengembalian DECIMAL NOT NULL,
+	bagi_hasil DECIMAL NOT NULL,
+	biaya_administrasi DECIMAL NOT NULL,
+	keterangan CHAR(15) NOT NULL,
+	id_anggota VARCHAR(50) NOT NULL,
+	FOREIGN KEY(id_anggota) REFERENCES tb_anggota(id_anggota)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)ENGINE=INNODB;
+
+CREATE TABLE tb_biaya_operasional(
+	id_biaya_operasional VARCHAR(50) NOT NULL PRIMARY KEY,
+	tanggal_transaksi DATE NOT NULL,
+	jenis_beban VARCHAR(50) NOT NULL,
+	keterangan TEXT NOT NULL,
+	biaya DECIMAL NOT NULL
+)ENGINE=INNODB;
+
+CREATE TABLE tb_biaya_asset(
+	id_biaya_asset VARCHAR(50) NOT NULL PRIMARY KEY,
+	kode_inventaris VARCHAR(50) NOT NULL,
+	sumber CHAR(9) NOT NULL,
+	keterangan TEXT NOT NULL,
+	biaya DECIMAL NOT NULL
+)ENGINE=INNODB;
