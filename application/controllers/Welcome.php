1c1ba9b7b0388227d6cd1ff64ef1a7f8805f1bb7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-14 13:26:18 +0700

    format

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,3 +1,10 @@
+/**
+ * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Date:   2016-08-14 13:25:58
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-14 13:26:07
+ */
+
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
