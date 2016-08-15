ab9af52ab42968bd1de29f4d8511d048ea71560a
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-15 12:14:31 +0700

    update anggota

--- application/models/Anggota.php
+++ application/models/Anggota.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
 /**
  * @Author: Aviv Arifian D
  * @Date:   2016-08-15 11:44:57
  * @Last Modified by:   Aviv Arifian D
- * @Last Modified time: 2016-08-15 11:49:31
+ * @Last Modified time: 2016-08-15 12:00:44
  */
 
 class Anggota extends CI_Model
 {
-	//ambil data anggota
-	public function ambilAnggota()
-	{
-		return $this->db->get('tb_anggota');
-	}
+    //ambil data anggota
+    public function ambilAnggota()
+    {
+        return $this->db->get('tb_anggota');
+    }
+
 }
