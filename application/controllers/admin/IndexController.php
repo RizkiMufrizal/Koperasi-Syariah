4e99a19b8da5e22f7a47201330164a0cde2af049
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-15 23:41:09 +0700

    update constructor

--- application/controllers/admin/IndexController.php
+++ application/controllers/admin/IndexController.php
@@ -3,11 +3,23 @@
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-15 15:04:35
  * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-15 15:06:44
+ * @Last Modified time: 2016-08-15 23:39:13
  */
 
 class IndexController extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $session = $this->session->userdata('loggedIn');
+        if ($session == false) {
+            $this->session->set_flashdata('pesan', 'maaf, anda belum melakukan login');
+            return redirect('/');
+        }
+    }
+
     /**
      * Halaman Dasboard Admin
      * @param string $value [description]
