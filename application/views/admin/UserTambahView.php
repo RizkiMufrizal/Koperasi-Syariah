6c197bc45562e4a02d5d352b94f7684652a4b543
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-16 02:23:04 +0700

    tambah view tambah user

---
+++ application/views/admin/UserTambahView.php
@@ -0,0 +1,58 @@
+<!--
+/**
+ * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Date:   2016-08-16 01:41:30
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-16 02:09:47
+ */
+-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Halaman Admin</title>
+        <?php $this->load->view('layout/CssLayout')?>
+    </head>
+    <body>
+
+        <?php $this->load->view('layout/HeaderLayout')?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Tambah Data User</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+            	<div class="col-lg-12">
+            		<form method="post" action="<?php echo base_url(); ?>index.php/UserController/simpanUser">
+            			<div class="form-group">
+            				<label for="username">Username</label>
+    						<input name="username" type="text" class="form-control" id="username" placeholder="masukkan username anda">
+            			</div>
+            			<div class="form-group">
+            				<label for="password">Password</label>
+    						<input name="password" type="password" class="form-control" id="password" placeholder="masukkan password anda">
+            			</div>
+            			<div class="form-group">
+            				<label for="role">Role</label>
+    						<select name="role" id="role" class="form-control">
+  								<option value="ROLE_ADMIN">Role Admin</option>
+  								<option value="ROLE_USER">Role User</option>
+							</select>
+            			</div>
+            			<input type="hidden" name="<?php echo $name; ?>" value="<?php echo $hash; ?>" />
+            			<button type="submit" class="btn btn-primary">Simpan</button>
+            			<a href="<?php echo base_url(); ?>index.php/UserController/index">
+            				<button type="button" class="btn btn-warning">Batal</button>
+            			</a>
+            		</form>
+            	</div>
+            </div>
+        </div>
+
+        <?php $this->load->view('layout/JsLayout')?>
+    </body>
+</html>
