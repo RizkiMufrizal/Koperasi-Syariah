a400a623f405d7d21df8d94a804aaf72d323efe7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-15 23:01:14 +0700

    tambah index controller untuk user login

---
+++ application/controllers/user/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Date:   2016-08-15 16:18:04
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-15 22:59:36
+ */
+
+class IndexController extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $session = $this->session->userdata('loggedIn');
+        if ($session == false) {
+            $this->session->set_flashdata('pesan', 'maaf, anda belum melakukan login');
+            return redirect('/');
+        }
+    }
+
+    /**
+     * Halaman index user setelah login
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        return $this->load->view('user/IndexView');
+    }
+}
