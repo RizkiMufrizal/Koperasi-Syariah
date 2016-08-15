bdc9ede978b33c452bf809344d70df376c50b9ae
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-16 02:21:51 +0700

    update layout js

--- application/views/layout/JsLayout.php
+++ application/views/layout/JsLayout.php
@@ -3,9 +3,11 @@
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-15 14:41:25
  * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-15 15:09:49
+ * @Last Modified time: 2016-08-16 01:54:08
  */
 -->
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.1.0.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js"></script>
