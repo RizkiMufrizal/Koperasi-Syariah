1497f74fdd42599a07fc76de3ec8fde559c1f1f8
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-15 15:26:12 +0700

    Update Pembiayaan

--- application/models/Pembiayaan.php
+++ application/models/Pembiayaan.php
@@ -2,28 +2,22 @@
 /**
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-15 12:56:42
- * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-15 12:58:50
+ * @Last Modified by:   Aviv Arifian D
+ * @Last Modified time: 2016-08-15 15:25:06
  */
 
 class Pembiayaan extends CI_Model
 {
-    /**
-     * ambil data pembiayaan
-     * @return [type] [description]
-     */
+    //Untuk Ambil Data Pembiayaan
     public function ambilPembiayaan()
     {
         return $this->db->get('tb_pembiayaan');
     }
 
-    /**
-     * simpan data pembiayaan
-     * @param  [type] $pembiayaan [description]
-     * @return [type]             [description]
-     */
-    public function simpanPembiayaan($pembiayaan)
+    //Untuk Simpan Pembiayaan
+    public function simpanPembiayaan($data)
     {
-        $this->db->insert('tb_pembiayaan', $pembiayaan);
+        $this->db->insert('tb_pembiayaan', $data);
     }
+
 }
