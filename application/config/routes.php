c98a1dab1891d5d3ea789c7d4758423f043e7322
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-15 15:33:21 +0700

    ubah default route

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:    my-controller/index    -> my_controller/index
 |        my-controller/my-method    -> my_controller/my_method
  */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'UserController/login';
 
 $route['admin']['GET']         = 'admin/IndexController/index';
 $route['404_override']         = '';
