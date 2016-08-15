303531a2cd4ba85394b375d80d71cb78e468a522
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-15 13:07:07 +0700

    tambah controller pembiayaan

---
+++ application/controllers/admin/PembiayaanController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Date:   2016-08-15 13:06:14
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-15 13:06:21
+ */
