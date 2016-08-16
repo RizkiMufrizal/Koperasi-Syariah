7be8c2b569cd541af6362bb87c9dff49b2473bc4
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-16 12:55:33 +0700

    update model pinjaman instan

--- application/models/PinjamanInstan.php
+++ application/models/PinjamanInstan.php
@@ -1,23 +1,14 @@
 <?php
-<<<<<<< HEAD
 
 /**
  * @Author: Aviv Arifian D
  * @Date:   2016-08-16 06:21:55
  * @Last Modified by:   Aviv Arifian D
- * @Last Modified time: 2016-08-16 06:44:53
-=======
-/**
- * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
- * @Date:   2016-08-16 01:32:59
- * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-16 01:37:20
->>>>>>> 471736f4196b0351260e86f66338c6aa1d50494b
+ * @Last Modified time: 2016-08-16 07:36:11
  */
 
 class PinjamanInstan extends CI_Model
 {
-<<<<<<< HEAD
     //Ambil Data Pinjaman Instan
     public function ambilPinjamanInstan()
     {
@@ -44,47 +35,4 @@ class PinjamanInstan extends CI_Model
         $this->db->update('tb_peminjaman_instan', $data);
     }
 
-=======
-    /**
-     * function untuk ambil data pinjaman instan
-     * @return [type] [description]
-     */
-    public function ambilPinjamanInstan()
-    {
-        return $this->db->get('tb_pinjaman_instan')->result();
-    }
-
-    /**
-     * function untuk mengambil 1 data pinjaman instan
-     * @param  [type] $idPinjamanInstan [description]
-     * @return [type]                   [description]
-     */
-    public function ambilSatuPinjamanInstan($idPinjamanInstan)
-    {
-        $this->db->whete('id_pinjaman_instan', $idPinjamanInstan);
-        return $this->db->get('tb_pinjaman_instan')->result();
-    }
-
-    /**
-     * function untuk simpan data pinjaman instan
-     * @param  [type] $pinjamanInstan [description]
-     * @return [type]                 [description]
-     */
-    public function simpanPinjamanInstan($pinjamanInstan)
-    {
-        $this->db->insert('tb_pinjaman_instan', $pinjamanInstan);
-    }
-
-    /**
-     * function untuk update pinjaman instan
-     * @param  [type] $idPinjamanInstan [description]
-     * @param  [type] $pinjamanInstan   [description]
-     * @return [type]                   [description]
-     */
-    public function updatePinjamanInstan($idPinjamanInstan, $pinjamanInstan)
-    {
-        $this->db->where('id_pinjaman_instan', $idPinjamanInstan);
-        $this->db->update('tb_pinjaman_instan', $pinjamanInstan);
-    }
->>>>>>> 471736f4196b0351260e86f66338c6aa1d50494b
 }
