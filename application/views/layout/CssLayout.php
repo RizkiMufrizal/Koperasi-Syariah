f06a03f9b015c9519506c4768a368daea170936f
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-16 19:37:56 +0700

    tambah datepicker

--- application/views/layout/CssLayout.php
+++ application/views/layout/CssLayout.php
@@ -3,11 +3,12 @@
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-15 14:41:25
  * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-16 01:53:27
+ * @Last Modified time: 2016-08-16 18:23:10
  */
 -->
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/dataTables.bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/jquery.datetimepicker.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/sb-admin-2.css">
