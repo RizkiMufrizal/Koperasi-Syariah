e75e976c7fe2b4ce89f04b22eb68e1b763b3f0d4
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-16 07:00:17 +0700

    Update Anggota

--- application/models/Anggota.php
+++ application/models/Anggota.php
@@ -4,7 +4,7 @@
  * @Author: Aviv Arifian D
  * @Date:   2016-08-15 11:44:57
  * @Last Modified by:   Aviv Arifian D
- * @Last Modified time: 2016-08-15 15:24:47
+ * @Last Modified time: 2016-08-16 06:27:57
  */
 
 class Anggota extends CI_Model
@@ -15,28 +15,30 @@ class Anggota extends CI_Model
         return $this->db->get('tb_anggota');
     }
 
-    //Ambil 1 Data Anggota
-    public function getSingleAnggota($id_anggota)
-    {
-        return $query = $this->db->get_where('tb_anggota', array('id_anggota' => $id_anggota));
-    }
-
-    //Hapus Data Anggota
-    public function hapusAnggota($id_anggota)
-    {
-        $this->db->delete('tb_anggota', array('id_anggota' => $id_anggota));
-    }
-
     //Simpan Data Anggota
     public function simpanAnggota($data)
     {
         $this->db->insert('tb_anggota', $data);
     }
 
+    //Ambil 1 Data Anggota
+    public function ambilSatuAnggota($id_anggota)
+    {
+        $this->db->where('id_anggota', $id_anggota);
+        return $this->db->get('tb_anggota')->result();
+    }
+
+    //Untuk Update Data Anggota
     public function updateAnggota($id_anggota, $data)
     {
         $this->db->where('id_anggota', $id_anggota);
         $this->db->update('tb_anggota', $data);
+    }
+
+    //Hapus Data Anggota
+    public function hapusAnggota($id_anggota)
+    {
+        $this->db->delete('tb_anggota', array('id_anggota' => $id_anggota));
     }
 
 }
