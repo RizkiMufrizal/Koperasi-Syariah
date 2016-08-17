091d551be0ccbf857e71723f83a32552323cd7ce
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-17 21:08:01 +0700

    Update BiayaOperasional

--- application/models/BiayaOperasional.php
+++ application/models/BiayaOperasional.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-16 01:27:33
- * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-16 01:30:32
+ * @Last Modified by:   Aviv Arifian D
+ * @Last Modified time: 2016-08-17 20:36:38
  */
 
 class BiayaOperasional extends CI_Model
@@ -15,6 +15,7 @@ class BiayaOperasional extends CI_Model
      */
     public function ambilBiayaOperasional($value = '')
     {
+        $this->db->order_by("tanggal_transaksi", "desc");
         return $this->db->get('tb_biaya_operasional')->result();
     }
 
