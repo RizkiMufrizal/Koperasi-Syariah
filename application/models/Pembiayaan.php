ff4e0bd58941b9f6477d1c139615dcfb3b415d31
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-17 11:47:26 +0700

    update tambah result dan ambil berdasarkan yang terbaru

--- application/models/Pembiayaan.php
+++ application/models/Pembiayaan.php
@@ -3,22 +3,30 @@
 /**
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-15 12:56:42
- * @Last Modified by:   Aviv Arifian D
- * @Last Modified time: 2016-08-15 16:14:54
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-17 11:27:40
  */
 
 class Pembiayaan extends CI_Model
 {
     //Untuk Ambil Data Pembiayaan
-    public function ambilPembiayaan()
+    public function ambilPembiayaan($idAnggota)
     {
-        return $this->db->get('tb_pembiayaan');
+        $this->db->where('id_anggota', $idAnggota);
+        return $this->db->get('tb_pembiayaan')->result();
     }
 
     //Untuk Simpan Pembiayaan
     public function simpanPembiayaan($data)
     {
         $this->db->insert('tb_pembiayaan', $data);
+    }
+
+    public function ambilPembiayaanTerbaru($idAnggota)
+    {
+        $this->db->order_by('tanggal_peminjaman', 'DESC');
+        $this->db->where('id_anggota', $idAnggota);
+        return $this->db->get('tb_pembiayaan')->result();
     }
 
 }
