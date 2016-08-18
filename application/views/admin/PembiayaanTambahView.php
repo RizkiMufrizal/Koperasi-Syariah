0177f519453a060bdd2ab0da9fc7231a64186171
Aviv Arifian D <avivkuliah@gmail.com>
2016-08-18 14:36:38 +0700

    Update PembiayaanTambahView

--- application/views/admin/PembiayaanTambahView.php
+++ application/views/admin/PembiayaanTambahView.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-17 09:49:57
- * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-17 22:05:47
+ * @Last Modified by:   Aviv Arifian D
+ * @Last Modified time: 2016-08-18 13:14:23
  */
 -->
 
@@ -31,17 +31,17 @@
 
                         <div class="form-group">
                             <label for="datetimepicker1">Tanggal Peminjaman</label>
-                            <input id="datetimepicker1" name="tanggal_peminjaman" class="form-control" placeholder="masukkan Tanggal Peminjaman anda">
+                            <input id="datetimepicker1" name="tanggal_peminjaman" class="form-control" placeholder="masukkan Tanggal Peminjaman anda" required>
                         </div>
 
                         <div class="form-group">
                             <label for="datetimepicker2">Tanggal Jatuh Tempo</label>
-                            <input id="datetimepicker2" name="tanggal_jatuh_tempo" class="form-control" placeholder="masukkan Tanggal Jatuh Tempo anda">
+                            <input id="datetimepicker2" name="tanggal_jatuh_tempo" class="form-control" placeholder="masukkan Tanggal Jatuh Tempo anda" required>
                         </div>
 
                         <div class="form-group">
                             <label for="pembiayaan">Pembiayaan</label>
-                            <input type="text" id="pembiayaan" name="pembiayaan" class="form-control angka" placeholder="masukkan Pembiayaan anda">
+                            <input type="text" id="pembiayaan" name="pembiayaan" class="form-control angka" placeholder="masukkan Pembiayaan anda" required>
                         </div>
 
                         <div class="form-group">
@@ -56,7 +56,7 @@
 
                         <div class="form-group">
                             <label for="margin">Margin</label>
-                            <input type="number" id="margin" name="margin" class="form-control" placeholder="masukkan Margin anda">
+                            <input type="number" id="margin" name="margin" class="form-control" placeholder="masukkan Margin anda" required>
                         </div>
 
                         <input type="hidden" name="<?php echo $name; ?>" value="<?php echo $hash; ?>" />
