e0c8d1b581397acef90fdf95ea6b2d9414a62348
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-18 22:48:49 +0700

    tambah query berdasarkan dua tanggal

--- application/models/BiayaOperasional.php
+++ application/models/BiayaOperasional.php
@@ -3,7 +3,7 @@
  * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
  * @Date:   2016-08-16 01:27:33
  * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-18 18:27:52
+ * @Last Modified time: 2016-08-18 22:45:17
  */
 
 class BiayaOperasional extends CI_Model
@@ -27,5 +27,16 @@ class BiayaOperasional extends CI_Model
     public function simpanBiayaOperasional($biayaOperasional)
     {
         $this->db->insert('tb_biaya_operasional', $biayaOperasional);
+    }
+
+    /**
+     * ambil data biaya operasional berdasarkan dua tanggal
+     * @param  [type] $tanggalAwal  [description]
+     * @param  [type] $tanggalAkhir [description]
+     * @return [type]               [description]
+     */
+    public function ambilBiayaOperasionalBerdasarkanDuaTanggal($tanggalAwal, $tanggalAkhir)
+    {
+        return $this->db->query("SELECT biaya FROM tb_biaya_operasional WHERE tanggal_transaksi BETWEEN " . "'" . $tanggalAwal . "' AND '" . $tanggalAkhir . "'")->result();
     }
 }
