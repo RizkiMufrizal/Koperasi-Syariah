aaf5d458267186baee2a9420cc084b347f557883
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-19 18:58:40 +0700

    validasi user yang sudah ada

--- application/controllers/admin/AnggotaController.php
+++ application/controllers/admin/AnggotaController.php
@@ -4,7 +4,7 @@
  * @Author: Aviv Arifian D
  * @Date:   2016-08-15 13:58:58
  * @Last Modified by:   RizkiMufrizal
- * @Last Modified time: 2016-08-18 14:59:19
+ * @Last Modified time: 2016-08-19 18:56:13
  */
 
 class AnggotaController extends CI_Controller
@@ -58,113 +58,118 @@ class AnggotaController extends CI_Controller
     //Untuk Menyimpan Data Anggota Ke Dalam Tabel Anggota
     public function simpanAnggota()
     {
+        $userCheck = $this->User->ambilSatuUser($this->input->post('username'));
 
-        $role = $this->session->userdata('role');
-        if ($role == 'ROLE_USER') {
-            $this->session->set_flashdata('pesan', 'maaf, anda tidak memiliki hak akses untuk halaman tersebut');
-            return redirect('/');
+        if ($userCheck != null) {
+            $this->session->set_flashdata('pesan', 'maaf, username telah digunakan');
+            return redirect('admin/AnggotaController/tambahAnggota');
         } else {
-            $pisah   = explode('/', $this->input->post('tanggal_lahir'));
-            $urutan  = array($pisah[2], $pisah[1], $pisah[0]);
-            $satukan = implode('-', $urutan);
-
-            $nama                    = $this->input->post('nama');
-            $tanggal_pendaftaran     = date('Y-m-d');
-            $telepon                 = $this->input->post('telepon');
-            $tempat_lahir            = $this->input->post('tempat_lahir');
-            $tanggal_lahir           = $satukan;
-            $jenis_kelamin           = $this->input->post('jenis_kelamin');
-            $rembug                  = $this->input->post('rembug');
-            $setoran_awal            = $this->input->post('setoran_awal');
-            $replaceRpSetoranAwal    = str_replace("Rp ", "", explode(".", $setoran_awal)[0]);
-            $replaceTitikSetoranAwal = str_replace(",", "", $replaceRpSetoranAwal);
-
-            $alamat   = $this->input->post('alamat');
-            $status   = $this->input->post('status');
-            $username = $this->input->post('username');
-
-            $kode_rembug = "";
-            $id          = 0;
-            $kode        = "";
-            if ($rembug == "Dewan") {
-                $kode_rembug = "DW";
-            } else if ($rembug == "Surya Legi Nyata") {
-                $kode_rembug = "SL";
-            } else if ($rembug == "Pratama") {
-                $kode_rembug = "PR";
-            } else if ($rembug == "Maju Bersama") {
-                $kode_rembug = "MB";
-            } else if ($rembug == "Arafah") {
-                $kode_rembug = "AR";
-            } else if ($rembug == "Bunga Raya") {
-                $kode_rembug = "BR";
-            } else if ($rembug == "Mawar") {
-                $kode_rembug = "MW";
-            } else if ($rembug == "Al-Falah") {
-                $kode_rembug = "AF";
-            } else if ($rembug == "Kartun") {
-                $kode_rembug = "KT";
-            } else if ($rembug == "Bougenville") {
-                $kode_rembug = "BG";
-            } else if ($rembug == "Matahari") {
-                $kode_rembug = "MT";
-            } else if ($rembug == "Teratai") {
-                $kode_rembug = "TR";
-            } else if ($rembug == "Mina") {
-                $kode_rembug = "MN";
-            } else if ($rembug == "Asoka") {
-                $kode_rembug = "AS";
-            } else if ($rembug == "LaaTanza") {
-                $kode_rembug = "LT";
-            }
-
-            if ($this->Anggota->ambilCountAnggota() == 0) {
-                $kode = $kode_rembug . '-0001';
-                $id   = 1;
+            $role = $this->session->userdata('role');
+            if ($role == 'ROLE_USER') {
+                $this->session->set_flashdata('pesan', 'maaf, anda tidak memiliki hak akses untuk halaman tersebut');
+                return redirect('/');
             } else {
-                $hasilSubString = substr($this->Anggota->ambilDataAnggotaTerbaru()[0]->id_anggota, 3);
-                $noUrutTest     = $hasilSubString + 1;
-                $id             = $noUrutTest;
-                if ($noUrutTest >= 2 && $noUrutTest <= 9) {
-                    $kode = $kode_rembug . '-000' . $noUrutTest;
-                } else if ($noUrutTest >= 10 && $noUrutTest <= 99) {
-                    $kode = $kode_rembug . '-00' . $noUrutTest;
-                } else if ($noUrutTest >= 100 && $noUrutTest <= 999) {
-                    $kode = $kode_rembug . '-0' . $noUrutTest;
-                } else if ($noUrutTest >= 1000 && $noUrutTest <= 9999) {
-                    $kode = $kode_rembug . '-' . $noUrutTest;
+                $pisah   = explode('/', $this->input->post('tanggal_lahir'));
+                $urutan  = array($pisah[2], $pisah[1], $pisah[0]);
+                $satukan = implode('-', $urutan);
+
+                $nama                    = $this->input->post('nama');
+                $tanggal_pendaftaran     = date('Y-m-d');
+                $telepon                 = $this->input->post('telepon');
+                $tempat_lahir            = $this->input->post('tempat_lahir');
+                $tanggal_lahir           = $satukan;
+                $jenis_kelamin           = $this->input->post('jenis_kelamin');
+                $rembug                  = $this->input->post('rembug');
+                $setoran_awal            = $this->input->post('setoran_awal');
+                $replaceRpSetoranAwal    = str_replace("Rp ", "", explode(".", $setoran_awal)[0]);
+                $replaceTitikSetoranAwal = str_replace(",", "", $replaceRpSetoranAwal);
+
+                $alamat   = $this->input->post('alamat');
+                $status   = $this->input->post('status');
+                $username = $this->input->post('username');
+
+                $kode_rembug = "";
+                $id          = 0;
+                $kode        = "";
+                if ($rembug == "Dewan") {
+                    $kode_rembug = "DW";
+                } else if ($rembug == "Surya Legi Nyata") {
+                    $kode_rembug = "SL";
+                } else if ($rembug == "Pratama") {
+                    $kode_rembug = "PR";
+                } else if ($rembug == "Maju Bersama") {
+                    $kode_rembug = "MB";
+                } else if ($rembug == "Arafah") {
+                    $kode_rembug = "AR";
+                } else if ($rembug == "Bunga Raya") {
+                    $kode_rembug = "BR";
+                } else if ($rembug == "Mawar") {
+                    $kode_rembug = "MW";
+                } else if ($rembug == "Al-Falah") {
+                    $kode_rembug = "AF";
+                } else if ($rembug == "Kartun") {
+                    $kode_rembug = "KT";
+                } else if ($rembug == "Bougenville") {
+                    $kode_rembug = "BG";
+                } else if ($rembug == "Matahari") {
+                    $kode_rembug = "MT";
+                } else if ($rembug == "Teratai") {
+                    $kode_rembug = "TR";
+                } else if ($rembug == "Mina") {
+                    $kode_rembug = "MN";
+                } else if ($rembug == "Asoka") {
+                    $kode_rembug = "AS";
+                } else if ($rembug == "LaaTanza") {
+                    $kode_rembug = "LT";
                 }
+
+                if ($this->Anggota->ambilCountAnggota() == 0) {
+                    $kode = $kode_rembug . '-0001';
+                    $id   = 1;
+                } else {
+                    $hasilSubString = substr($this->Anggota->ambilDataAnggotaTerbaru()[0]->id_anggota, 3);
+                    $noUrutTest     = $hasilSubString + 1;
+                    $id             = $noUrutTest;
+                    if ($noUrutTest >= 2 && $noUrutTest <= 9) {
+                        $kode = $kode_rembug . '-000' . $noUrutTest;
+                    } else if ($noUrutTest >= 10 && $noUrutTest <= 99) {
+                        $kode = $kode_rembug . '-00' . $noUrutTest;
+                    } else if ($noUrutTest >= 100 && $noUrutTest <= 999) {
+                        $kode = $kode_rembug . '-0' . $noUrutTest;
+                    } else if ($noUrutTest >= 1000 && $noUrutTest <= 9999) {
+                        $kode = $kode_rembug . '-' . $noUrutTest;
+                    }
+                }
+
+                $hash = $this->bcrypt->hash_password($this->input->post('password'));
+                $user = array(
+                    'username' => $this->input->post('username'),
+                    'password' => $hash,
+                    'role'     => 'ROLE_USER',
+                );
+
+                $this->User->simpanUser($user);
+
+                $data = array(
+                    'id_anggota'          => $kode,
+                    'id'                  => $id,
+                    'nama'                => $nama,
+                    'tanggal_pendaftaran' => $tanggal_pendaftaran,
+                    'telepon'             => $telepon,
+                    'tempat_lahir'        => $tempat_lahir,
+                    'tanggal_lahir'       => $tanggal_lahir,
+                    'jenis_kelamin'       => $jenis_kelamin,
+                    'rembug'              => $rembug,
+                    'setoran_awal'        => $replaceTitikSetoranAwal,
+                    'alamat'              => $alamat,
+                    'status'              => $status,
+                    'username'            => $username);
+
+                $this->Anggota->simpanAnggota($data);
+
+                return redirect('admin/AnggotaController/index');
             }
-
-            $hash = $this->bcrypt->hash_password($this->input->post('password'));
-            $user = array(
-                'username' => $this->input->post('username'),
-                'password' => $hash,
-                'role'     => 'ROLE_USER',
-            );
-
-            $this->User->simpanUser($user);
-
-            $data = array(
-                'id_anggota'          => $kode,
-                'id'                  => $id,
-                'nama'                => $nama,
-                'tanggal_pendaftaran' => $tanggal_pendaftaran,
-                'telepon'             => $telepon,
-                'tempat_lahir'        => $tempat_lahir,
-                'tanggal_lahir'       => $tanggal_lahir,
-                'jenis_kelamin'       => $jenis_kelamin,
-                'rembug'              => $rembug,
-                'setoran_awal'        => $replaceTitikSetoranAwal,
-                'alamat'              => $alamat,
-                'status'              => $status,
-                'username'            => $username);
-
-            $this->Anggota->simpanAnggota($data);
-
-            return redirect('admin/AnggotaController/index');
         }
-
     }
 
     //Ambil 1 Data Anggota Lalu Menampilkan Halaman Edit Anggota
