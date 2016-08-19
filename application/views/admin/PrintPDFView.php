748d3d0b81e9188fdc36b91bf588a3c99bd89c89
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-19 19:54:55 +0700

    tambah view laporan

---
+++ application/views/admin/PrintPDFView.php
@@ -0,0 +1,8 @@
+<!--
+/**
+ * @Author: Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Date:   2016-08-19 19:54:09
+ * @Last Modified by:   RizkiMufrizal
+ * @Last Modified time: 2016-08-19 19:54:15
+ */
+-->
